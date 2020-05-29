c5d56e02b0bbd840f0e422773f14fc79ccc7faf1
Rajat Gupta <rajat295gupta@gmail.com>
2020-05-29 13:14:28 +0530

    eader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar>
+        <ToolBar>
+          <Typography variant="h4">
+            Build Your Resume
+          </Typography>
+        </ToolBar>
+      </AppBar>
     </div>
   );
 }
